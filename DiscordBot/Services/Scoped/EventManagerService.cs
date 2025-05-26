fc4908aa6f07c4dc06749112a3cc18216e9f72c7
Lev Kotovskiy <leb.beliiii@gmail.com>
2025-05-26 04:08:21 +0300

    Базовые методы

--- DiscordBot/Services/Scoped/EventManagerService.cs
+++ DiscordBot/Services/Scoped/EventManagerService.cs
@@ -1,7 +1,54 @@
-﻿using DiscordBot.Services.Scoped.Interfaces;
+﻿using System;
+using System.Threading.Tasks;
+using DiscordBot.Database.Entities;
+using DiscordBot.Services.Scoped.Interfaces;
 
 namespace DiscordBot.Services.Scoped;
 
 public class EventManagerService : IEventManagerService
 {
+    public Task<bool> TryAddUpdateEventTemplateAsync(EventTemplateEntity eventTemplateEnt)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<EventTemplateEntity> GetEventTemplateAsync(ulong guildId, int eventTemplateId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> TryDeleteEventTemplateAsync(ulong guildId, int eventTemplateId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> TryAddUpdateEventAsync(EventEntity eventEnt)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<EventTemplateEntity> GetEventAsync(ulong guildId, int eventId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> TryDeleteEventAsync(ulong guildId, int eventId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> TryAddUpdateEventRepeatabilityAsync(EventRepeatabilityEntity eventRepeatEnt)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<EventRepeatabilityEntity> GetEventRepeatabilityAsync(ulong guildId, int eventRepeatId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> TryDeleteEventRepeatabilityAsync(ulong guildId, int eventRepeatId)
+    {
+        throw new NotImplementedException();
+    }
 }
--- DiscordBot/Services/Scoped/Interfaces/IEventManagerService.cs
+++ DiscordBot/Services/Scoped/Interfaces/IEventManagerService.cs
@@ -1,5 +1,19 @@
-﻿namespace DiscordBot.Services.Scoped.Interfaces;
+﻿using System.Threading.Tasks;
+using DiscordBot.Database.Entities;
+
+namespace DiscordBot.Services.Scoped.Interfaces;
 
 public interface IEventManagerService
 {
+    Task<bool> TryAddUpdateEventTemplateAsync(EventTemplateEntity eventTemplateEnt);
+    Task<EventTemplateEntity> GetEventTemplateAsync(ulong guildId, int eventTemplateId);
+    Task<bool> TryDeleteEventTemplateAsync(ulong guildId, int eventTemplateId);
+
+    Task<bool> TryAddUpdateEventAsync(EventEntity eventEnt);
+    Task<EventTemplateEntity> GetEventAsync(ulong guildId, int eventId);
+    Task<bool> TryDeleteEventAsync(ulong guildId, int eventId);
+
+    Task<bool> TryAddUpdateEventRepeatabilityAsync(EventRepeatabilityEntity eventRepeatEnt);
+    Task<EventRepeatabilityEntity> GetEventRepeatabilityAsync(ulong guildId, int eventRepeatId);
+    Task<bool> TryDeleteEventRepeatabilityAsync(ulong guildId, int eventRepeatId);
 }
